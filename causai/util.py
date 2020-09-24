e753a74c96f4a3b382c97939476c2800a2b06e6c
DanielhCarranza <ingbiodanielh@gmail.com>
2020-09-23 19:18:02 -0700

    add dowloand function

--- causai/util.py
+++ causai/util.py
@@ -6,6 +6,40 @@ from tqdm import tqdm
 from typing import Union
 from pathlib import Path
 from urllib.request import urlopen, urlretrieve
+from concurrent.futures import as_completed, ThreadPoolExecutor
+
+class TqdmUpTo(tqdm):
+    """From https://github.com/tqdm/tqdm/blob/master/examples/tqdm_wget.py"""
+
+    def update_to(self, blocks=1, bsize=1, tsize=None):
+        """
+        Parameters
+        ----------
+        blocks : int, optional
+            Number of blocks transferred so far [default: 1].
+        bsize  : int, optional
+            Size of each block (in tqdm units) [default: 1].
+        tsize  : int, optional
+            Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            self.total = tsize  
+        self.update(blocks * bsize - self.n)  
 
 
+def download_url(url, filename):
+    """Download a file from url to filename, with a progress bar."""
+    with TqdmUpTo(unit="B", unit_scale=True, unit_divisor=1024, miniters=1) as t:
+        urlretrieve(url, filename, reporthook=t.update_to, data=None)  # nosec
+
+
+def download_urls(urls, filenames):
+    """Download urls to filenames in a multi-threaded way."""
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(urlretrieve, url, filename) for url, filename in zip(urls, filenames)]
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            try:
+                future.result()
+            except Exception as e:  
+                print("Error", e)
 
